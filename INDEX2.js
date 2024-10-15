2a089290538efccb28f640cd94d99f82409224a6
Crewtlc <longhourswaltair@gmail.com>
2024-10-15 18:16:04 +0530

    Add files via upload

---
+++ INDEX2.js
@@ -0,0 +1,11 @@
+document.getElementById('MOBILE CHECK').addEventListener('click', function() {
+    window.location.href = "https://crewtlc.github.io/mobilecheck/"; 
+document.getElementById('BA CHECK').addEventListener('click', function() {
+        window.location.href = "https://crewtlc.github.io/bacheck/"; 
+document.getElementById('SIGNAL ASPECT/SPEED TEST').addEventListener('click', function() {
+            window.location.href = "https://crewtlc.github.io/signalspeed/"; 
+document.getElementById('AMBUSH CHECK').addEventListener('click', function() {
+                window.location.href = "https://crewtlc.github.io/ambush//"; 
+
+
+});
