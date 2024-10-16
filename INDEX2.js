1072f4a765f6dcd9ed8690d011b54bb15790ba58
Crewtlc <longhourswaltair@gmail.com>
2024-10-16 10:23:09 +0530

    Update INDEX2.js

--- INDEX2.js
+++ INDEX2.js
@@ -1,11 +1,6 @@
-document.getElementById('MOBILE CHECK').addEventListener('MOBILE CHECK', function() {
-    window.location.href = "https://crewtlc.github.io/mobilecheck/"; 
-document.getElementById('BA CHECK').addEventListener('BA CHECK', function() {
-        window.location.href = "https://crewtlc.github.io/bacheck/"; 
-document.getElementById('SIGNAL ASPECT/SPEED TEST').addEventListener('SIGNAL ASPECT/SPEED TEST', function() {
-            window.location.href = "https://crewtlc.github.io/signalspeed/"; 
-document.getElementById('AMBUSH CHECK').addEventListener('AMBUSH CHECK', function() {
-                window.location.href = "https://crewtlc.github.io/ambush//"; 
-
-
+document.querySelectorAll('.railway-button').forEach(button => {
+  button.addEventListener('click', function() {
+    alert(this.textContent + ' button clicked!');
+  });
 });
+
